3a67272d3ec495b4106af5e4fc5f37ac62e06f67
Denys Pavlenko <de.pavlenko@gmail.com>
2021-07-28 20:44:13 +0300

    Change hero image; Add paralax effect

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -2,19 +2,40 @@ import React from 'react';
 import Typography from 'components/typography';
 import styles from './index.module.sass';
 
-const Hero = () => (
-  <div className={styles.hero}>
-    <div className={styles['hero-content']}>
-      <div className={styles['hero-image']} />
-      <Typography
-        component="h1"
-        align="center"
-        className={styles['hero-title']}
-      >
-        The Rick and Morty App
-      </Typography>
+const Hero = () => {
+  const contEl = React.useRef(null);
+  const titleEl = React.useRef(null);
+
+  React.useEffect(() => {
+    const contentHeight = contEl.current.offsetHeight;
+
+    const parallaxEffect = () => {
+      const scrollTop = window.scrollY;
+      if (contentHeight >= scrollTop) {
+        contEl.current.style.transform = `translateY(-${scrollTop / 5}px)`;
+        titleEl.current.style.transform = `translateY(-${scrollTop / 10}px)`;
+      }
+    };
+
+    window.addEventListener('scroll', parallaxEffect);
+    return () => window.removeEventListener('scroll', parallaxEffect);
+  }, []);
+
+  return (
+    <div className={styles.hero}>
+      <div className={styles['hero-content']} ref={contEl}>
+        <div className={styles['hero-image']} />
+        <Typography
+          component="h1"
+          align="center"
+          className={styles['hero-title']}
+          ref={titleEl}
+        >
+          The Rick and Morty App
+        </Typography>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Hero;
