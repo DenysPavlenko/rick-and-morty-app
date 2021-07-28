9ca2a52f6fb1c1671d766848891e378837f2444b
Denys Pavlenko <de.pavlenko@gmail.com>
2021-07-28 15:22:38 +0300

    Connect store to the app. Refactor profiles component in order to use redux

--- src/components/profiles/index.jsx
+++ src/components/profiles/index.jsx
@@ -1,26 +1,32 @@
+/* eslint-disable no-shadow */
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import PropTypes from 'prop-types';
+// Redux
+import { fetchProfilesRequest } from 'redux/profiles/actions';
+import selectProfiles from 'redux/profiles/selectors';
 // Components
 import Profile from 'components/profile';
 // Styles
 import styles from './index.module.sass';
 
-const Profiles = () => {
-  const [profiles, setProfiles] = React.useState([]);
-
+const Profiles = ({ fetchProfilesRequest, profiles }) => {
   React.useEffect(() => {
-    fetch('https://rickandmortyapi.com/api/character/?page=1')
-      .then((res) => res.json())
-      .then((res) => setProfiles(res.results));
+    fetchProfilesRequest();
   }, []);
+
+  if (profiles.error) {
+    return <div className={styles.profiles}>profiles.error</div>;
+  }
 
   return (
     <div className={styles.profiles}>
       <div className={styles['profiles-row']}>
-        {profiles.length === 0 ? (
-          <p>loading</p>
-        ) : (
+        {profiles.loading && <p>loading</p>}
+        {profiles.data && (
           <>
-            {profiles.map((profile) => (
+            {profiles.data.map((profile) => (
               <div key={profile.id} className={styles['profiles-col']}>
                 <Profile
                   image={profile.image}
@@ -41,4 +47,21 @@ const Profiles = () => {
   );
 };
 
-export default Profiles;
+const mapStateToProps = createStructuredSelector({
+  profiles: selectProfiles,
+});
+
+const mapDispatchToProps = {
+  fetchProfilesRequest,
+};
+
+Profiles.propTypes = {
+  fetchProfilesRequest: PropTypes.func.isRequired,
+  profiles: PropTypes.shape({
+    data: PropTypes.array,
+    loading: PropTypes.bool,
+    error: PropTypes.string,
+  }).isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profiles);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+// Store
+import store from 'redux/store';
+// Components
 import App from './App';
+// Styles
 import './index.sass';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
