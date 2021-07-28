b92901fd855ceb1ac34c1f1af0a394e48c77d2fa
Denys Pavlenko <de.pavlenko@gmail.com>
2021-07-28 19:54:31 +0300

    Refactor hero view

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Typography from 'components/typography';
-import heroImage from 'assets/images/hero-image.png';
 import styles from './index.module.sass';
 
 const Hero = () => (
   <div className={styles.hero}>
     <div className={styles['hero-content']}>
-      <img src={heroImage} alt="hero" className={styles['hero-image']} />
+      <div className={styles['hero-image']} />
       <Typography
         component="h1"
         align="center"
