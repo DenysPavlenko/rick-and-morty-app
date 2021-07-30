5718b68a42336fa37a604837cea7e55b3d4a834b
Denys Pavlenko <de.pavlenko@gmail.com>
2021-07-30 15:20:37 +0300

    Add error boundry to app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+// Components
+import ErrorBoudry from 'components/error-boundry';
 // Pages
 import ProfilesPage from 'pages/profiles-page';
 import PageNotFound from 'pages/page-not-found';
@@ -8,16 +10,18 @@ import { ThemeProvider } from 'context/theme-context';
 
 const App = () => (
   <div className="app">
-    <ThemeProvider>
-      <Switch>
-        <Route exact strict path={['/', '/:id(\\d+)']}>
-          <ProfilesPage />
-        </Route>
-        <Route path="*">
-          <PageNotFound />
-        </Route>
-      </Switch>
-    </ThemeProvider>
+    <ErrorBoudry>
+      <ThemeProvider>
+        <Switch>
+          <Route exact strict path={['/', '/:id(\\d+)']}>
+            <ProfilesPage />
+          </Route>
+          <Route path="*">
+            <PageNotFound />
+          </Route>
+        </Switch>
+      </ThemeProvider>
+    </ErrorBoudry>
   </div>
 );
 
