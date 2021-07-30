d5bf4f402e9111fe819d5c5813575c97e5da28ee
Denys Pavlenko <de.pavlenko@gmail.com>
2021-07-30 13:07:48 +0300

    Install enzyme-to-json

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,11 @@
 import Enzyme from 'enzyme';
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+import toJson from 'enzyme-to-json';
 
 Enzyme.configure({
   adapter: new EnzymeAdapter(),
   disableLifecycleMethods: true,
   snapshotSerializers: ['enzyme-to-json/serializer'],
 });
+
+global.toJson = toJson;
